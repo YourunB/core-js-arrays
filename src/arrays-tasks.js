44536b0751f38a854aebe3534635b163faa8e4f1
Yury Butskevich <bxyura@gmail.com>
2024-01-09 15:06:46 +0300

    feat: complete generateOdds func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -374,8 +374,9 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const res = new Array(len).fill();
+  return res.map((v, i) => 2 * i + 1);
 }
 
 /**
