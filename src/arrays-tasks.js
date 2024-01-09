fd449f43b718ecd5b255dddf0311a1a6da9e39ab
Yury Butskevich <bxyura@gmail.com>
2024-01-09 13:12:01 +0300

    feat: complete getTail func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -214,8 +214,11 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  let res;
+  if (n !== 0) res = arr.slice(-n);
+  else res = [];
+  return res;
 }
 
 /**
