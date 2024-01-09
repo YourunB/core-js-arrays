32cbc5d7539c82c77e963362ddcd933f1fe16e96
Yury Butskevich <bxyura@gmail.com>
2024-01-09 12:41:11 +0300

    feat: complete getAverage func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -137,8 +137,10 @@ function getStringsLength(arr) {
  *   getAverage[ 1, 10, 100, 1000 ]  => 277,75
  *   getAverage[ 2, 3, 3 ]  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const avg = arr.reduce((acc, el) => acc + el) / arr.length;
+  return Math.round(avg * 100) / 100;
 }
 
 /**
