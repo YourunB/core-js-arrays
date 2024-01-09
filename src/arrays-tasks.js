e296ad6fc68ae897a3c9cb496644f1552c88966f
Yury Butskevich <bxyura@gmail.com>
2024-01-09 16:43:05 +0300

    feat: complete getIndicesOfOddNumbers func

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -445,8 +445,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const res = [];
+  numbers.map((v, i) => (v % 2 !== 0 ? res.push(i) : v));
+  return res;
 }
 
 /**
